6ea2efdd91cffaa73996c7b640a85a74e3314f56
Dickson Diku <dixsonlukis@gmail.com>
2021-03-03 18:21:17 -0500

    tests added

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -11,7 +11,55 @@ class GildedRoseTest {
         Item[] items = new Item[] { new Item("foo", 0, 0) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("fixme2", app.items[0].name);
+        assertEquals("foo", app.items[0].name);
+        //Approvals.verify(app.items[0].name);
+        
     }
+    
+    
+    @Test
+    void decreaseQuality() {
+    	
+    	Item[] items = new Item[] { new Item("+5 Dexterity Vest", 10, 20) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(19, app.items[0].quality);
+        assertEquals(9, app.items[0].sellIn);
+    	
+    }
+    
+    @Test
+    void increaseQualityForAged_Brie() {
+    	
+    	Item[] items = new Item[] { new Item("Aged Brie", 2, 0) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(1, app.items[0].quality);
+        assertEquals(1, app.items[0].sellIn);
+    	
+    }
+    
+    @Test
+    void increaseQualityElixirMongoose() {
+    	
+    	Item[] items = new Item[] { new Item("Elixir of the Mongoose", 5, 0)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(0, app.items[0].quality);
+        assertEquals(4, app.items[0].sellIn);
+    	
+    }
+    
 
+    @Test
+    void updateQualitySulfurasHandRagnaros(){
+    	
+    	Item[] items = new Item[] { new Item("Sulfuras, Hand of Ragnaros", 0, 80)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(80, app.items[0].quality);
+        assertEquals(0, app.items[0].sellIn);
+    	
+    }
+    
 }
