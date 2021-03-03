5e53081235ae65ca426d09d6a1baab0009595e05
halilraba <57082927+halilraba@users.noreply.github.com>
2021-03-03 18:43:03 -0500

    Added last 6 test cases

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -61,5 +61,88 @@ class GildedRoseTest {
         assertEquals(0, app.items[0].sellIn);
     	
     }
+
+    @Test
+    void should_decrease_quality_twice_as_fast_when_sell_date_has_passed(){
+
+        Item[] items = new Item[] { new Item("Elixir of the Mongoose", 0, 80)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(78, app.items[0].quality);
+        assertEquals(-1, app.items[0].sellIn);
+
+    }
+
+    @Test
+    void should_increase_quality_as_selling_value_approaches(){
+
+        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(21, app.items[0].quality);
+        assertEquals(14, app.items[0].sellIn);
+    }
+
+    @Test
+    void should_increase_quality_by_2_as_selling_value_approaches(){
+
+        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 10, 20)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(22, app.items[0].quality);
+        assertEquals(9, app.items[0].sellIn);
+    }
+
+    @Test
+    void should_increase_quality_by_3_as_selling_value_approaches(){
+
+        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 5, 40)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(43, app.items[0].quality);
+        assertEquals(4, app.items[0].sellIn);
+    }
+
+    @Test
+    void should_quality_never_be_more_than_50(){
+
+        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(50, app.items[0].quality);
+        assertEquals(4, app.items[0].sellIn);
+
+    }
+
+    @Test
+    void should_quality_never_be_more_than_50_for_Aged_Brie(){
+
+        Item[] items = new Item[] { new Item("Aged Brie", 8, 50)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(50, app.items[0].quality);
+        assertEquals(7, app.items[0].sellIn);
+    }
+
+    @Test
+    void should_quality_never_be_less_than_0(){
+
+        Item[] items = new Item[] { new Item("Aged Tree", 8, 0)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(0, app.items[0].quality);
+        assertEquals(7, app.items[0].sellIn);
+    }
+
+    @Test
+    void should_quality_drops_to_0_after_concert(){
+
+        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 0, 49)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(0, app.items[0].quality);
+        assertEquals(-1, app.items[0].sellIn);
+    }
+
     
 }
